9d9a5a1cc7b04b1ab15dc46e4b7ca868a20ecc3c
ariyaratne_mhb2017 <ariyaratne_mhb2017@instance-1.c.brave-server-247606.internal>
2019-07-31 08:32:34 +0500

    Working on restful API for LIMS

---
+++ README.md
@@ -0,0 +1,1 @@
+Informaiton System for Managing Project Concept Papers at the Management, Development & Planning Unit of the Ministry of Health, Nutrition and Indeginious Medicine, Sri Lanka
---
+++ src/main/java/entity/WebUserRole.java
@@ -0,0 +1,16 @@
+package entity;
+
+/**
+ *
+ * @author Dr M H B Ariyaratne
+ */
+public enum WebUserRole {
+    System_Administrator,
+    Super_User,
+    User,
+    Institution_User,
+    Institution_Administrator,
+    Authority_User,
+    Authority_Admin,
+
+}
